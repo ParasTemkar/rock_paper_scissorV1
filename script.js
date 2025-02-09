561f589438403a7b3763c090853370259fed65ef
ParasTemkar <parastemkar0204@gmail.com>
2025-02-09 15:00:22 +0530

    Add various scenarios. Improved previous code.
    Wrote logic for winning or losing scenarios for a single round. Improved for when prompt is canceled.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//to get computer choice
 function getComputerChoice() {
     return Math.floor(Math.random()*9)+1;
   }
@@ -15,7 +16,41 @@ else if ([7,8,9].includes(randomInt)){
     computerChoice = "scissor";
 }
 
-let humanChoice = function getHumanChoice(){
-    return prompt("Enter rock, paper or scissor","rock");
+// to get human choice
+function getHumanChoice() {
+    let humanChoice = prompt("Enter rock, paper or scissor: ");
+
+    if (humanChoice === null) {
+        alert("No input provided. Please try again.");
+        return null;
+    }
+    return humanChoice.toLowerCase();
+}
+let humanChoice = getHumanChoice();
+
+
+if (humanChoice === computerChoice) {
+    alert("Computer's choice: " + computerChoice + "\nYour choice: " + humanChoice + "\nTIE!!!");
+} else if (humanChoice === null) {
+    // No action needed
 }
 
+//winning scenarios
+else if (computerChoice === "rock" && humanChoice === "paper") {
+    alert("Computer's choice: " + computerChoice + "\nYour choice: " + humanChoice + "\nYOU WON!!!");
+} else if (computerChoice === "scissor" && humanChoice === "rock") {
+    alert("Computer's choice: " + computerChoice + "\nYour choice: " + humanChoice + "\nYOU WON!!!");
+} else if (computerChoice === "paper" && humanChoice === "scissor") {
+    alert("Computer's choice: " + computerChoice + "\nYour choice: " + humanChoice + "\nYOU WON!!!");
+}
+
+//losing scenarios
+else if (computerChoice === "rock" && humanChoice === "scissor") {
+    alert("Computer's choice: " + computerChoice + "\nYour choice: " + humanChoice + "\nYOU LOST.");
+} else if (computerChoice === "paper" && humanChoice === "rock") {
+    alert("Computer's choice: " + computerChoice + "\nYour choice: " + humanChoice + "\nYOU LOST.");
+} else if (computerChoice === "scissor" && humanChoice === "paper") {
+    alert("Computer's choice: " + computerChoice + "\nYour choice: " + humanChoice + "\nYOU LOST.");
+} else {
+    alert("Enter rock, paper or scissor");
+}
